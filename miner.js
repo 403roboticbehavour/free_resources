6ab24fe6ac8c2e927c739d6515f1a8145a37221b
403 <nikhilpatel46548@gmail.com>
2026-03-26 10:48:14 +0530

    Add files via upload

---
+++ miner.js
@@ -0,0 +1,5 @@
+#!/bin/sh
+./xmrig --config =/home/nikhil/Desktop/chill/cryptography/xmrig-6.25.0/config.json  
+#!/bin/sh
+echo "Mining script started" > /sdcard/mining_log.txt
+./xmrig --config=/home/nikhil/Desktop/chill/cryptography/xmrig-6.25.0/config.json
